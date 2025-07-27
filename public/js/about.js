5be2a9ec564490713aa2a47402eceb166f5354a8
Dipali-32 <dipali9607@gmail.com>
2025-07-27 17:01:29 +0530

    Initial commit - portfolio website

---
+++ public/js/about.js
@@ -0,0 +1,15 @@
+
+//   // Wait until DOM is ready
+//   document.addEventListener("DOMContentLoaded", () => {
+//     const cards = document.querySelectorAll('.about-card');
+
+//     cards.forEach(card => {
+//       card.addEventListener('click', () => {
+//         // Remove "selected" class from all cards
+//         cards.forEach(c => c.classList.remove('selected'));
+//         // Add to clicked one
+//         card.classList.add('selected');
+//       });
+//     });
+//   });
+
